abbe829d25919c93fa0b6e5d0a6e3932e8d607c0
antonywillson <antonywillson@localhost>
2023-11-30 09:10:18 +0700

    hilangin main

--- src/main/Main.java
+++ src/main/Main.java
@@ -20,67 +20,16 @@ import model.LoginGrid;
 
 public class Main extends Application{
 
-	// Menu
-	MenuBar menuBar;	
-	Menu menu1;
-	MenuItem menuItem1;
-	MenuItem menuItem2;
-	
-	// Tool Bar
-	ToolBar toolBar;
-	
-	// Button
-	Button logoutButton;
-	
-	// Label
-	Label welcomeLabel;
-	
-	// Region
-	Region regionToolBar;
 	
 
 
 	private void initialize() {
-		
-		
-		
-		//Menu
-		menuBar = new MenuBar();
-		menu1 = new Menu();
-		menuItem1 = new MenuItem();
-		menuItem2 = new MenuItem();
-		
-
-		
-		// Tool Bar
-		toolBar = new ToolBar();
-	
-		// Button
-		logoutButton = new Button();
-		
-		// Label
-		welcomeLabel = new Label();
-
-		
-		// Region
-		regionToolBar = new Region();
 
 	}
 	
 	private void components() {
 		
-		// Label
-		welcomeLabel.setText("Hi, Antony");
 		
-		// button
-		logoutButton.setText("Logout");
-	
-		
-		// Tool bar
-		HBox.setHgrow(regionToolBar, Priority.ALWAYS);
-		toolBar.getItems().addAll(logoutButton);
-		toolBar.getItems().add(regionToolBar);
-		toolBar.getItems().add(welcomeLabel);
 	}
 	
 	private void arrangeComponents() {
