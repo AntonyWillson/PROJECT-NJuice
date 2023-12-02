f5f04e9500fda19256d3833ab441071be61935bf
antonywillson <antonywillson@localhost>
2023-12-02 15:14:22 +0700

    Manage Products

--- src/model/ManageProducts.java
+++ src/model/ManageProducts.java
@@ -7,6 +7,7 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -19,6 +20,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -65,6 +67,9 @@ public class ManageProducts extends GridPane implements EventHandler<ActionEvent
 	//Text field & Area
 	TextField nameField;
 	TextArea descArea;
+	
+	//Alert
+	Alert alert;
 
 
 	//Label
@@ -118,6 +123,11 @@ public class ManageProducts extends GridPane implements EventHandler<ActionEvent
 
 		//Combo box
 		productId = new ComboBox<>();
+		
+		//Alert
+		alert = new Alert(AlertType.ERROR);
+		alert.setTitle("Error");
+		alert.setContentText("Please fill all the field");
 
 		// Spinner
 		price = new Spinner<>();
@@ -252,6 +262,9 @@ public class ManageProducts extends GridPane implements EventHandler<ActionEvent
 	void SetEvent() {
 		menuItem1.setOnAction(this);
 		menuItem3.setOnAction(this);
+		insertBtn.setOnAction(this);
+		removeBtn.setOnAction(this);
+		updateBtn.setOnAction(this);
 	}
 	public ManageProducts(Stage mainStage) {
 		Initialize();
@@ -276,6 +289,18 @@ public class ManageProducts extends GridPane implements EventHandler<ActionEvent
 		}else if (e.getSource() == menuItem3) {
 			LoginGrid login = new LoginGrid(mainStage);
 			login.show();
+		}else if (e.getSource() == insertBtn) {
+			if (nameField.getText().isEmpty() || descArea.getText().isEmpty()) {
+				alert.show();
+			}
+		}else if (e.getSource() == removeBtn) {
+			if (nameField.getText().isEmpty() || descArea.getText().isEmpty()) {
+				alert.show();
+			}
+		}else if (e.getSource() == updateBtn) {
+			if (nameField.getText().isEmpty() || descArea.getText().isEmpty()) {
+				alert.show();
+			}
 		}
 
 	}
